1d431031286ed9d592b313465573bf0c876dd153
Roman Lizunov <rlizunoff@gmail.com>
2022-07-04 11:17:07 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const itemEl = document.querySelector('ul#categories');
+const itemElList = document.querySelector('li.item');
+console.log(itemEl);
+console.log(itemElList);
+console.log('Number of categories:', itemEl.childElementCount);
+console.log('Category: ', itemElList.firstElementChild.textContent);  
+// console.log('Elements:', itemE.);
+// childNodes.h2.textContent li.item
