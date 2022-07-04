6dd1855d3281f0809c3e35fc4ec46830cf3a4c23
Roman Lizunov <rlizunoff@gmail.com>
2022-07-04 15:24:50 +0300

    123

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,21 @@
 const itemEl = document.querySelector('ul#categories');
 const itemElList = document.querySelector('li.item');
+
 console.log(itemEl);
 console.log(itemElList);
 console.log('Number of categories:', itemEl.childElementCount);
 console.log('Category: ', itemElList.firstElementChild.textContent);  
 // console.log('Elements:', itemE.);
 // childNodes.h2.textContent li.item
+let total = 0;
+function name(params) {
+    total += 1;
+};
+// console.log(name(itemEl));
+// console.log(total);
+console.log('оно', itemEl.childNodes.length);
+alert(document.getElementById('mymenu').childNodes.length);
+
+    // способ 2:
+    alert(document.getElementById('mymenu').getElementsByTagName('li').length);
+
