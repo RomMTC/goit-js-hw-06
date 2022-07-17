87c8e2feab90b3ef2f8254d2d0ce52471f8cfe21
Roman Lizunov <rlizunoff@gmail.com>
2022-07-17 19:44:38 +0300

    9 correct

--- js/task-09.js
+++ js/task-09.js
@@ -4,11 +4,12 @@ function getRandomHexColor() {
 // Напиши скрипт, который изменяет цвета фона элемента < body > через инлайн стиль 
 // при клике на button.change - color и выводит значение цвета в span.color.
 const btnColor = document.querySelector('.change-color');
-const spanColor = document.querySelector('.color');
 
 const onChangeColor = () => {
   const changeColor = getRandomHexColor()
   document.body.style.background = changeColor;
+
+  const spanColor = document.querySelector('.color');
   spanColor.textContent = changeColor;
 };
 btnColor.addEventListener('click', onChangeColor);
